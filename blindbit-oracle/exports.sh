2e786bd578284853d27c77a3cb12bbe36283b523
setavenger <github@snblago.com>
2024-10-20 21:22:07 +0200

    fixing wrong hidden service configuration

--- blindbit-oracle/exports.sh
+++ blindbit-oracle/exports.sh
@@ -1,2 +1,5 @@
 export APP_BLINDBIT_ORACLE_NODE_IP="10.21.23.4"
-export APP_BLINDBIT_ORACLE_NODE_PORT="8080"
+export APP_BLINDBIT_ORACLE_NODE_PORT="5728"
+
+rpc_hidden_service_file="${EXPORTS_TOR_DATA_DIR}/app-${EXPORTS_APP_ID}-rpc/hostname"
+export APP_BLINDBIT_ORACLE_API_HIDDEN_SERVICE="$(cat "${rpc_hidden_service_file}" 2>/dev/null || echo "notyetset.onion")"
