b22e82d35474547fb67293b07b35b142b05fb710
setavenger <github@snblago.com>
2024-10-20 21:22:07 +0200

    fixing wrong hidden service configuration

--- blindbit-scan/exports.sh
+++ blindbit-scan/exports.sh
@@ -1,2 +1,5 @@
-# export APP_BLINDBIT_SCAN_NODE_IP="10.21.23.4"
+export APP_BLINDBIT_SCAN_NODE_IP="10.21.23.5"
 export APP_BLINDBIT_SCAN_NODE_PORT="5729"
+
+rpc_hidden_service_file="${EXPORTS_TOR_DATA_DIR}/app-${EXPORTS_APP_ID}-rpc/hostname"
+export APP_BLINDBIT_SCAN_API_HIDDEN_SERVICE="$(cat "${rpc_hidden_service_file}" 2>/dev/null || echo "notyetset.onion")"
